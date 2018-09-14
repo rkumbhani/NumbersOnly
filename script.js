e5f88ebe48031718d8be5d70327fa34dfa4d902d
Ruhi Kumbhani <ruhi.kumbhani@gmail.com>
2018-09-14 14:51:21 +0530

    added in some js and css

---
+++ script.js
@@ -0,0 +1,17 @@
+// var for input
+var number = document.getElementById("numberInput").value;
+Submitted = "true";
+// length < 20 characters
+if (number.length > 20) {
+    alert("The number must no more than 20 characters");  
+    Submitted = "false";
+}
+// validate - numbers, -, . 
+if (number ) {
+    alert("The number must be a number");  
+    Submitted = "false";
+}
+
+// local storage - mongo
+// if input is empty, user time submitted is recorded
+// display all with time and number and user number
